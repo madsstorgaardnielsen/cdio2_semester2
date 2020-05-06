b9ae3cab4eb80fcbe94abdb0f451150f4cdb9476
Mads Storgaard-Nielsen <madssn@hotmail.com>
2020-05-06 12:25:10 +0200

    refact

--- src/main/java/dto/UserDTO.java
+++ src/main/java/dto/UserDTO.java
@@ -12,6 +12,15 @@ public class UserDTO implements Serializable {
     String role;
     private static final long serialVersionUID = 4545864587995944260L;
 
+    public UserDTO(int userId, String firstName, String lastName, String initials, String cpr, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.initials = initials;
+        this.cpr = cpr;
+        this.password = password;
+    }
+
     public String getRole() {
         return role;
     }
@@ -68,15 +77,6 @@ public class UserDTO implements Serializable {
     }
 
     public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserDTO(int userId, String firstName, String lastName, String initials, String cpr, String password) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.initials = initials;
-        this.cpr = cpr;
         this.password = password;
     }
 
