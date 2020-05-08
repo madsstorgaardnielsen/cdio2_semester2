a8ed43de0da23ead94ab145eed97ba38a9f1545b
Mads Storgaard-Nielsen <madssn@hotmail.com>
2020-05-08 15:21:04 +0200

    added usercontroller, will refactor later

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import dao.UserDAO;
+import dto.UserDTO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class UserController {
+    private UserDAO userDAO;
+    private UserDTO userDTO;
+
+    public UserController() throws SQLException {
+        this.userDAO = new UserDAO();
+        this.userDTO = new UserDTO();
+    }
+
+    public void updateUserName(int id, String newFirstname, String newLastname) throws SQLException, IOException {
+        userDTO = userDAO.getUserById(id);
+        userDTO.setFirstName(newFirstname);
+        userDTO.setLastName(newLastname);
+        userDAO.updateUserName(userDTO);
+    }
+
+
+    public void updateUserInitials(int id,String newInitials) throws SQLException, IOException {
+        userDTO = userDAO.getUserById(id);
+        userDTO.setInitials(newInitials);
+        userDAO.updateUserInitials(userDTO);
+    }
+
+    public void updateUserCpr(int id,String newCpr) throws SQLException, IOException {
+        userDTO = userDAO.getUserById(id);
+        userDTO.setCpr(newCpr);
+        userDAO.updateUserCpr(userDTO);
+    }
+
+    public void updateUserPassword(int id,String newPassword) throws SQLException, IOException {
+        userDTO = userDAO.getUserById(id);
+        userDTO.setPassword(newPassword);
+        userDAO.updateUserPassword(userDTO);
+    }
+}
