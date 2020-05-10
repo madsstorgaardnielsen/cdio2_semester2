9f6bfca73275e9e1f0747f464ebf541d057f399a
Jonas Henriksen <56156281+JonasHenriksen123@users.noreply.github.com>
2020-05-10 13:50:56 +0200

    modified edit user

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -21,7 +21,7 @@ function addUser() {
 function loadUsers() {
     $("#userTableBody").empty();
     //Hints: $.each(data, function(i, element){ } iterates over a JSON-collection (data).
-    // $('').append('html'), appends html to an html elemenut.
+    // $('').append('html'), appends html to an html element.
     Agent.getJson("rest/user",
         function (data) {
             $.each(data, function () {
@@ -37,14 +37,14 @@ function loadUsers() {
 
 function generateUserHTML(user) {
     return '<tr> <td class = userId>' + user.userId + '</td>' +
-        '<td class = role>' + user.role + '</td>' +
+        '<td class ="role">' + user.role + '</td>' +
         '<td class = firstName>' + user.firstName + '</td>' +
         '<td class = lastName>' + user.lastName + '</td>' +
         '<td class = ini>' + user.initials + '</td>' +
         '<td class = cpr>' + user.cpr + '</td>' +
         '<td class = password>' + user.password + '</td>' +
-        '<td> <form action="javascript:deleteUser(' + user.userId + ')"> <button type="submit">Delete</button> </form>  </td>' +
-        '<td> <form action="UpdateUser.html"> <button type="submit">Edit</button> </form> </td> </tr>'
+        '<td> <form action="javascript:deleteUser(' + user.userId + ')"> <button class="deletebutton">Delete</button> </form>  </td>' +
+        '<td class = edit> <button class="editbutton">Edit</button> </td> </tr>'
 }
 
 function deleteUser(userId) {
@@ -68,25 +68,14 @@ function searchforUser() {
 )
 }
 
-function updateUser() {
-        var userInfo = {}
-        userInfo.userId = $("#userid").val();
-        userInfo.firstName = $("#firstname").val();
-        userInfo.lastName = $("#lastname").val();
-        userInfo.initials = $("#initials").val();
-        userInfo.cpr = $("#cpr").val();
-        userInfo.password = $("#password").val();
-        userInfo.role = $("#roller").val();
+function updateUser(userInfo) {
         Agent.putJson('rest/user', userInfo, function (data) {
-            $("#Message").empty();
-            $("#Message").append('<label>Bruger opdateret</label>');
-        }, function (xhr, statusmsg, errormsg) {
-            $("#Message").empty();
-            $("#Message").append('<label>Fejl opstod, bruger ikke opdateret</label>');
-
-            alert(xhr.responseJSON.message);
+            loadUsers();
+        }, function () {
+            window.alert("kunne ikke opdatere")
         });
 }
+
 
 
 
